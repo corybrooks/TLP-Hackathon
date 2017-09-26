71cc6ebf9813df2f53f05dbd96dbc9e27059c9c9
JamesRick <james.rick.1995@gmail.com>
2017-09-26 18:53:38 -0400

    Added LoginActivity, and MainActivity button to go to LoginActivity

--- app/src/main/java/com/example/rickjames/eraticators/MainActivity.java
+++ app/src/main/java/com/example/rickjames/eraticators/MainActivity.java
@@ -1,4 +1,7 @@
 package com.example.rickjames.eraticators;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.SignIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
